c21b1986ae9b25ca1dd8057e3e72d59af2a6aa1d
Mahim1997 <38731142+Mahim1997@users.noreply.github.com>
2019-06-15 03:30:57 +0600

    3 30 am

---
+++ CensorshipAnalyzer/src/ui/controllers/DNSRecordController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author viper
+ */
+public class DNSRecordController {
+
+    @FXML
+    private AnchorPane anchorPane;
+    
+}
