b586157e6ec00af98a74f6ccd33a803fcc2fc058
pranto47 <38744668+pranto47@users.noreply.github.com>
2019-07-28 09:11:44 +0600

    Update HTTPSDescription.py

--- CensorshipAnalyzer/New_Python_Programs/HTTPSDescription.py
+++ CensorshipAnalyzer/New_Python_Programs/HTTPSDescription.py
@@ -1,2 +1,21 @@
 class HTTPSDescription:
-	not_implemented = None
+	ip_address = []
+	https_response_code_local = []
+	https_response_code_tor = []
+	is_other_error = 0  # Default false
+	message_https = ""   # Default nothing
+
+	is_fin_bit_set = 0
+	is_rst_bit_set = 0
+	is_tls_handshake_failed = 0
+
+	redirection_history_local = []
+	redirection_history_tor = []
+
+	threshold_comarison = 0.300  # Default comparison threshold is 30 %
+	is_different_wrt_threshold = 0  # Default False
+	content_difference = 0.0
+	is_max_redirection_reached = 0
+
+	# Middle box hop count
+	middle_box_hop_count = 0
