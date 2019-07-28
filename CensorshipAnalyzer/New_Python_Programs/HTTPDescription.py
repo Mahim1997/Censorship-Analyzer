d894fae5c11b131ed44fd4577baada8144fb3259
pranto47 <38744668+pranto47@users.noreply.github.com>
2019-07-28 09:10:45 +0600

    Update HTTPDescription.py

--- CensorshipAnalyzer/New_Python_Programs/HTTPDescription.py
+++ CensorshipAnalyzer/New_Python_Programs/HTTPDescription.py
@@ -1,20 +1,21 @@
-
 class HTTPDescription:
-    # per ip
-    ip_address = None
-    is_no_response_GET_request = 0  # Default false
+	# per ip
+	ip_address = []
+	is_other_error = 0  # Default false
+	http_response_code_local = []
+	http_response_code_tor = []
+	message_http = ""   # Default nothing
 
-    message_http = ""   # Default nothing
+	is_fin_bit_set = 0
+	is_rst_bit_set = 0
 
-    is_fin_bit_set = 0
-    is_rst_bit_set = 0
+	redirection_history_local = []
+	redirection_history_tor = []
 
-    content_local_server = None
-    content_tor_browser = None
+	threshold_comarison = 0.300  # Default comparison threshold is 30 %
+	is_different_wrt_threshold = 0  # Default False
+	content_difference = 0.0
+	is_max_redirection_reached = 0
 
-    threshold_comarison = 0.300  # Default comparison threshold is 30 %
-    is_different_wrt_threshold = 0  # Default False
-    is_censored_HTTP = 0  # Default FALSE
-    
-    # Middle box hop count
-    middle_box_hop_count = 0
+	# Middle box hop count
+	middle_box_hop_count = 0
