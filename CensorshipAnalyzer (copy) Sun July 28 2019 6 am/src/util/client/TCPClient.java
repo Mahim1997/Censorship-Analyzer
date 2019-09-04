782b2cd5445468b5ca89897ff80d2b2e1833d2b8
pranto47 <38744668+pranto47@users.noreply.github.com>
2019-09-04 19:57:17 +0600

    Update TCPClient.java

--- CensorshipAnalyzer (copy) Sun July 28 2019 6 am/src/util/client/TCPClient.java
+++ CensorshipAnalyzer (copy) Sun July 28 2019 6 am/src/util/client/TCPClient.java
@@ -56,6 +56,39 @@ public class TCPClient {
         }
 
     }
+    
+    public int login_send_receive(String output) {
+        try {
+            s = new Socket(Config.SERVER_IP_ADDRESS, 7777);
+
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            pr = new PrintWriter(s.getOutputStream());
+            String strSend = null, strRecv = null;
+
+            strSend = output;
+
+            System.out.println("----------------- SENDING TO SERVER BEGIN -------------------------");
+
+            System.out.println(strSend);
+            System.out.println("----------------- SENDING TO SERVER DONE -------------------------");            
+            
+            pr.println(strSend);
+            pr.println();
+            pr.flush();
+            System.out.println("----------------- SENDING TO SERVER DONE -------------------------");    
+            strRecv = br.readLine();
+            System.out.println("Received data: "+strRecv);
+            int userid = Integer.parseInt(strRecv);
+            cleanUp();
+            return userid;
+
+        } catch (IOException e) {
+            System.out.println("Problem in connecting with the server in TCPClient.send(). Exiting main.");
+//            Notification.push("Error", "Cannot connect to System Server to send data", Notification.FAILURE, Pos.CENTER);
+            return 0;
+        }
+
+    }
 
     private void cleanUp() {
         try {
